1e40550679a1aebfde6c7954553e29c48d71fe13
solahidris <emailsolah@gmail.com>
2023-07-29 23:54:17 +0800

    Added Cover Art Button

--- src/components/HeaderCoverart.tsx
+++ src/components/HeaderCoverart.tsx
@@ -1,14 +1,28 @@
 // HeaderCoverart.tsx
 
-import LuffyGear5 from "../assets/luffyg5-min.png"
+import LuffyGear5 from "../assets/luffyg5-min.png";
 type HeaderCoverartProps = {};
 
 const HeaderCoverart: React.FC<HeaderCoverartProps> = () => {
   return (
     <div>
-        {/* Cover Art Image */}
+      {/* Title */}
+      <span className="text-3xl font-bold text-border-shadow tracking-wide text-white absolute z-10 top-20 left-5">
+        One Piece
+      </span>
+      {/* Description */}
+      <span className="text-xs text-white text-border-shadow absolute z-10 top-[120px] left-6 w-48">
+        The most anticipated episode featuring Luffy's Gear 5 evolution fighting
+        against Kaido
+      </span>
+      {/* Play & Info Button */}
+      <div className="flex gap-2 z-10 top-[184px] left-6 absolute font-semibold">
+        <button className="bg-stone-100 hover:bg-stone-300 rounded px-4 py-1 rounded ">► Play</button>
+        <button className="bg-stone-300/10 hover:bg-stone-500/10 rounded px-4 py-1 text-white">ⓘ More Info</button>
+      </div>
+      {/* Cover Art Image */}
       <img src={LuffyGear5} alt="gear5" className="w-full relative" />
-        {/* Black Gradient Bottom */}
+      {/* Black Gradient Bottom */}
       <div className="bg-gradient-to-t from-black to-transparent z-10 h-10 mt-[-40px] z-20 relative"></div>
     </div>
   );
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -6,15 +6,21 @@ type HomeProps = {};
 
 const Home: React.FC<HomeProps> = () => {
   return (
-    <div className="bg-stone-100 min-h-[100vh] relative">
+    <div className="bg-black min-h-[100vh]">
         <Header />
         <HeaderCoverart />
       
       {/* Content */}
-      <div className="absolute z-20">
-        <p>GEAR 5!</p>
-        <Notes />
+      <div className="text-white">
+        <p>New Releases</p>
+        <p>Continue Watching for User123</p>
+        <p>Popular on Netflix</p>
+        <p>Trending Now</p>
+        <p>Top 10 Movies in Malaysia Today</p>
+        <p>Top 10 TV Shows in Malaysia Today</p>
+        <p>Anime</p>
       </div>
+        <Notes />
     </div>
   );
 };
