671ac2767ba62159b5ffc67071bf191fa862850a
solahidris <emailsolah@gmail.com>
2023-08-01 16:23:36 +0800

    Installed react-router-dom

--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -1,19 +1,19 @@
 // Cards.tsx
 
 import useNewReleases from "../hooks/useNewReleases";
-import usePopularOnTMDb from "../hooks/usePopularOnTMDb";
-import useTop10MoviesInMalaysia from "../hooks/useTop10MoviesInMalaysia";
-import useTop10TVShowsInMalaysia from "../hooks/useTop10TVShowsInMalaysia";
-import useAnime from "../hooks/useAnime";
+// import usePopularOnTMDb from "../hooks/usePopularOnTMDb";
+// import useTop10MoviesInMalaysia from "../hooks/useTop10MoviesInMalaysia";
+// import useTop10TVShowsInMalaysia from "../hooks/useTop10TVShowsInMalaysia";
+// import useAnime from "../hooks/useAnime";
 
 type CardsProps = {};
 
 const Cards: React.FC<CardsProps> = () => {
   const { newReleases } = useNewReleases();
-  const { popularMovies } = usePopularOnTMDb();
-  const { top10Movies } = useTop10MoviesInMalaysia();
-  const { top10TVShows } = useTop10TVShowsInMalaysia();
-  const { anime } = useAnime();
+  // const { popularMovies } = usePopularOnTMDb();
+  // const { top10Movies } = useTop10MoviesInMalaysia();
+  // const { top10TVShows } = useTop10TVShowsInMalaysia();
+  // const { anime } = useAnime();
 
   return (
     <div>
@@ -32,9 +32,9 @@ const Cards: React.FC<CardsProps> = () => {
               ))}
         </div>
 
-        <p className="font-bold pb-2 pt-8">Popular on TMDb</p>
+        {/* <p className="font-bold pb-2 pt-8">Popular on TMDb</p>
         <div className="flex gap-2 overflow-x-auto">
-            {/* Display New Releases */}
+            Display New Releases
               {popularMovies.map((movie) => (
                 <img
                   key={movie.id}
@@ -43,11 +43,11 @@ const Cards: React.FC<CardsProps> = () => {
                   className="w-24 lg:w-52 rounded object-cover"
                 />
               ))}
-        </div>
+        </div> */}
 
-        <p className="font-bold pb-2 pt-8">Top 10 Movies in Malaysia Today</p>
+        {/* <p className="font-bold pb-2 pt-8">Top 10 Movies in Malaysia Today</p>
         <div className="flex gap-2 overflow-x-auto">
-            {/* Display New Releases */}
+            Display New Releases
               {top10Movies.map((movie) => (
                 <img
                   key={movie.id}
@@ -56,11 +56,11 @@ const Cards: React.FC<CardsProps> = () => {
                   className="w-24 lg:w-52 rounded object-cover"
                 />
               ))}
-        </div>
+        </div> */}
 
-        <p className="font-bold pb-2 pt-8">Top 10 TV Shows in Malaysia Today</p>
+        {/* <p className="font-bold pb-2 pt-8">Top 10 TV Shows in Malaysia Today</p>
         <div className="flex gap-2 overflow-x-auto">
-            {/* Display New Releases */}
+            Display New Releases
               {top10TVShows.map((movie) => (
                 <img
                   key={movie.id}
@@ -69,11 +69,11 @@ const Cards: React.FC<CardsProps> = () => {
                   className="w-24 lg:w-52 rounded object-cover"
                 />
               ))}
-        </div>
+        </div> */}
 
-        <p className="font-bold pb-2 pt-8">Anime</p>
+        {/* <p className="font-bold pb-2 pt-8">Anime</p>
         <div className="flex gap-2 overflow-x-auto">
-            {/* Display New Releases */}
+            Display New Releases
               {anime.map((movie) => (
                 <img
                   key={movie.id}
@@ -82,7 +82,7 @@ const Cards: React.FC<CardsProps> = () => {
                   className="w-24 lg:w-52 rounded object-cover"
                 />
               ))}
-        </div>
+        </div> */}
 
       </div>
     </div>
